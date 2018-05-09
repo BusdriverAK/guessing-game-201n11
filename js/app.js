f2d8d632a24e7222320f3d4efca81780db844c85
Mike K <m.kermes89@gmail.com>
2018-05-08 19:08:35 -0700

    touched index.html / started on app.js questions

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// questions to use this time
+// Am I from Seattle?
+// Have I visited more than 10 countries?
+// Have I seen over 100 artists in a single year?
+// Is celery the one food I hate?
+// What are my favorite sports to watch
+
+var answerCount = 0;
+
+//get user's name
+
+confirm('Are you ready to play a guessing game about me?');
+
+var uName = prompt('Alright, then. Who\'s taking the quiz?');
+console.log('The user\'s name is ' + uName);
